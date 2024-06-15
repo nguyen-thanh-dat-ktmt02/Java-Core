59c72b6284fd0feba9532602ed1024431b49babe
Dat <thanhdat2002tn@gmail.com>
2024-06-15 20:44:39 +0700

    create assignment-01

---
+++ src/main/java/Program.java
@@ -0,0 +1,8 @@
+public class Program {
+    public static void main(String[] args) {
+        Department department= new Department();
+        department.id= 1;
+        department.name="Bao ve";
+        System.out.println("Ten phong ban: " + department.name);
+    }
+}
